91988bf57bebf648973a7f883a3816e8734d5f15
Arteiimis <lionelym0987@gmail.com>
2026-02-05 23:05:31 +0800

    移除命令行选项组的注释，简化代码结构

--- src/cmd.h
+++ src/cmd.h
@@ -12,7 +12,6 @@ struct CmdParserResult {
 constexpr auto commandLineInit(int argc, char* argv[]) -> CmdParserResult {
   namespace po = boost::program_options;
 
-  // 通用选项 (General options)
   auto general = po::options_description("General options");
   general.add_options()                     //
     ("help,h", "produce help message")      //
@@ -20,7 +19,6 @@ constexpr auto commandLineInit(int argc, char* argv[]) -> CmdParserResult {
     ("yes,y", "automatic yes to prompts")   //
     ;
 
-  // 输入/输出选项 (Input/Output options)
   auto io = po::options_description("Input/Output options");
   io.add_options()                                                            //
     ("input,i", po::value<std::string>(), "input file or directory path")     //
@@ -29,21 +27,18 @@ constexpr auto commandLineInit(int argc, char* argv[]) -> CmdParserResult {
     ("recursive,R", "recursively search for video files in directories")      //
     ;
 
-  // 处理选项 (Processing options)
   auto processing = po::options_description("Processing options");
   processing.add_options()                                                    //
     ("type,t", po::value<std::string>(), "process type: video or picture")    //
     ("ffmpeg-path,f", po::value<std::string>(), "custom ffmpeg binary path")  //
     ;
 
-  // 文件操作选项 (File operation options)
   auto fileop = po::options_description("File operation options");
   fileop.add_options()                                        //
     ("overwrite", "overwrite existing files without prompt")  //
     ;
 
-  // 组合所有选项组
-  po::options_description all("Allowed options");
+  auto all = po::options_description("Allowed options");
   all.add(general).add(io).add(processing).add(fileop);
 
   auto vm = po::variables_map{};
